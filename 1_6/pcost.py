6b7fbe9b2e6ddaf6bfe8150850d809afec331612
adecora <alejandrodecora@gmail.com>
2024-01-05 20:08:05 +0100

    Exercise 1.6 and adding .gitignore

---
+++ 1_6/pcost.py
@@ -0,0 +1,21 @@
+# pcost.py
+
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename, 'r') as f:
+        for line in f:
+            fields = line.split()
+            try:
+                shares      = int(fields[1])
+                price       = float(fields[2])
+                total_cost += shares * price
+            
+            except ValueError as e:
+                print("Couldn't parse:", repr(line))
+                print("Reason:", e)
+
+    return total_cost
+
+
+if __name__ == '__main__':
+    print(portfolio_cost('Data/portfolio.dat'))
