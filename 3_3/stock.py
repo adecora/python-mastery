cfbba94f73fc76455728e372d64e8d85cd780a9e
adecora <alejandrodecora@gmail.com>
2024-04-13 23:25:05 +0200

    Exercise 3.3

---
+++ 3_3/stock.py
@@ -0,0 +1,21 @@
+# stock.py
+from decimal import Decimal
+
+class Stock:
+    types = (str, int, float)
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
+
+    def cost(self):
+        return self.shares * self.price
+
+
+class DStock(Stock):
+    types = (str, int, Decimal)
