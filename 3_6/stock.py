ac7b0390d89deac55703616b28849e0cfe21b8e2
adecora <alejandrodecora@gmail.com>
2024-06-26 00:33:10 +0200

    Exercise 3.6

---
+++ 3_6/stock.py
@@ -0,0 +1,56 @@
+# stock.py
+from decimal import Decimal
+
+class Stock:
+    __slots__ = ('name', '_shares', '_price')
+    _types = (str, int, float)
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*values)
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, self._types[1]):
+            raise TypeError(f'Expected a {self._types[1].__name__}')
+        elif value < 0:
+            raise ValueError('shares must be >= 0')
+        self._shares = value
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, self._types[2]):
+            raise TypeError(f'Expected a {self._types[2].__name__}')
+        elif value < 0:
+            raise ValueError('price must be >= 0')
+        self._price = value
+    
+    def __repr__(self):
+        return 'Stock({!r}, {!r}, {!r})'.format(self.name,
+                                                self.shares,
+                                                self.price)
+    
+    def __eq__(self, other):
+        return isinstance(other, Stock) and ((self.name, self.shares, self.price) ==
+                                             (other.name, other.shares, other.price))
+
+
+class DStock(Stock):
+    _types = (str, int, Decimal)
---
+++ 3_6/tableformat.py
@@ -0,0 +1,72 @@
+# tableformat.py
+import sys
+
+
+class TableFormatter:
+    def headings(self, headers):
+        raise NotImplementedError()
+
+    def row(self, rowdata):
+        raise NotImplementedError()
+
+
+class TextTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(' '.join('%10s' % h for h in headers))
+        print(('-'*10 + ' ')*len(headers))
+
+    def row(self, rowdata):
+        print(' '.join('%10s' % d for d in rowdata))
+
+
+class CSVTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(','.join(headers))
+    
+    def row(self, rowdata):
+        print(','.join(str(d) for d in rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    def headings(self, headings):
+        print('<tr>', end=' ')
+        for h in headings:
+            print('<th>%s</th>' % h, end=' ')
+        print('</tr>')
+    
+    def row(self, rowdata):
+        print('<tr>', end=' ')
+        for d in rowdata:
+            print('<td>%s</td>' % d, end=' ')
+        print('</tr>')
+
+
+class redirect_stdout:
+    def __init__(self, out_file):
+        self.out_file = out_file
+    
+    def __enter__(self):
+        self.stdout = sys.stdout
+        sys.stdout = self.out_file
+        return self.out_file
+
+    def __exit__(self, ty, val, tb):
+        sys.stdout = self.stdout    
+
+
+def create_formatter(name):
+    if name == 'text':
+        formatter_cls = TextTableFormatter
+    elif name == 'csv':
+        formatter_cls = CSVTableFormatter
+    elif name == 'html':
+        formatter_cls = HTMLTableFormatter
+    else:
+        raise RuntimeError('Unknown format %s' % name)
+    return formatter_cls()
+
+def print_table(portfolio, fields, formatter):
+    formatter.headings(fields)
+    for s in portfolio:
+        rowdata = [getattr(s, fieldname, None) for fieldname in fields]
+        formatter.row(rowdata)
