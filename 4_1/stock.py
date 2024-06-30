3fa836861ef670b7473456f85122ffa01bea4484
adecora <alejandrodecora@gmail.com>
2024-07-01 00:24:00 +0200

    Exercise 4.1

---
+++ 4_1/stock.py
@@ -0,0 +1,8 @@
+# stock.py
+class SimpleStock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+    def cost(self):
+        return self.shares * self.price
